70cce90fd2b2d0023a8087e509c57c6277e6fe66
anudeep-17 <54953858+anudeep-17@users.noreply.github.com>
2021-04-18 19:41:55 +0530

    website check

--- js-test.js
+++ js-test.js
@@ -5,7 +5,7 @@ document.write("<br/>");
 document.write("\n DEMONSTRATION OF BUTTON: \n");
 
 function change()
-    {
+    { 
         var x = "javascript is fun\n" + "<br/>";
         document.getElementById('results').innerHTML = x+x+x+x+x;
     }
